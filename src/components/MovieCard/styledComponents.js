fe6019a3635087574a1410b8a58ba6559a8ad46d
Phani <mysteriousphani@gmail.com>
2023-05-11 10:08:51 +0530

    Minor Changes in alignment

--- src/components/MovieCard/styledComponents.js
+++ src/components/MovieCard/styledComponents.js
@@ -1,25 +1,26 @@
 import {styled} from "styled-components"
 
 export const MovieCardBg = styled.li`
-margin:1em 0;
+margin:1em;
+min-width:150px;
 `
 
 export const MovieImage = styled.img`
-width:100%:
-max-width:650px;  
+width:100%:  
 margin-right:1em; 
 border-radius:8px;
 `
 
 export const MovieName = styled.h1`
-font-size:1.4em;
+font-size:0.8em;
 color:#fff;
-text-decoration:none;
+@media screen and (min-width:768px){
+    font-size:1.4em;
+}
 `
 
 export const MovieGenres = styled.h3`
 font-size:1em;
 color:#454545;
-text-decoration:none;
 `
 
